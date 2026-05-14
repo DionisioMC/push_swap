e1d52275e4a2a810d54a8d7ecdebad37d08a5cec
Dionisio Coelho <dcoelho@c1r10s8.42porto.com>
2026-05-14 14:39:46 +0100

    implementing pa, pb, ra, rb, rr, rra, rrb an rrr

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dcoelho <dcoelho@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 14:27:48 by dcoelho           #+#    #+#             */
-/*   Updated: 2026/05/13 15:25:25 by dcoelho          ###   ########.fr       */
+/*   Updated: 2026/05/14 14:37:31 by dcoelho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-int	ft_lstsize(t_list *lst);
+int		ft_lstsize(t_list *lst);
 t_list	*ft_lstnew(void *content);
-int	ft_lstsize(t_list *lst);
+int		ft_lstsize(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
